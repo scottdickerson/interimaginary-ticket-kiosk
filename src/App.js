d1c213b6e8e46980d9967c62195836993807f259
Scott Dickerson <swiftfoot@hotmail.com>
2020-12-12 11:57:01 -0600

    fix(bunnyimage): preload bunny animation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import TicketSurvey from './containers/TicketSurvey/TicketSurvey';
 import TicketSpinner from './containers/TicketSpinner/TicketSpinner';
 import { Helmet } from 'react-helmet';
 import bunnies from './containers/TicketSpinner/img/Bunnies.jpg';
+import Frame02 from './components/BunnyAnimation/img/Frame_02.jpg';
+import Frame03 from './components/BunnyAnimation/img/Frame_03.jpg';
+import Frame04 from './components/BunnyAnimation/img/Frame_04.jpg';
+import Frame05 from './components/BunnyAnimation/img/Frame_05.jpg';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
       <TicketPullScreen />
       <Helmet>
         <link rel="preload" as="image" href={bunnies} />
+        <link rel="preload" as="image" href={Frame02} />
+        <link rel="preload" as="image" href={Frame03} />
+        <link rel="preload" as="image" href={Frame04} />
+        <link rel="preload" as="image" href={Frame05} />
       </Helmet>
       <Switch>
         <Route path={ROUTES.MAINSCREEN} component={TicketSurvey} />
