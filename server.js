8f896b76aa101501d8f29ffb9067a5ab82b7b121
Scott Dickerson <swiftfoot@hotmail.com>
2021-08-28 14:27:24 -0500

    fix(cors): fix no-cors problem

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ let PrinterSwitch;
 // wait for the board to initialize and then setup the printerswitch
 board.on('ready', () => {
   console.log('board is ready!');
-  PrinterSwitch = new five.Relay(14);
+  PrinterSwitch = new five.Relay(7);
+  // PrinterSwitch = new five.Led(13);
+  // PrinterSwitch = new five.Pin(7);
 });
 const app = express();
 
@@ -18,11 +20,24 @@ const port = 3002;
 app.get('/open', (req, res) => {
   res.end('relay opened');
   PrinterSwitch.open();
+  // PrinterSwitch.low();
 });
 
 app.get('/close', (req, res) => {
   PrinterSwitch.close();
+  // PrinterSwitch.high();
   res.end('relay closed');
+});
+
+app.get('/blink', (req, res) => {
+  res.end('blinking onboard LED');
+  PrinterSwitch.blink();
+});
+
+// Handle 404 - Keep this as a last route
+app.use(function (req, res, next) {
+  res.status(404);
+  res.send('404: File Not Found');
 });
 
 app.listen(port, () => {
--- src/containers/TicketSpinner/TicketSpinner.jsx
+++ src/containers/TicketSpinner/TicketSpinner.jsx
@@ -6,9 +6,13 @@ import { withRouter } from 'react-router';
 import classNames from 'classnames';
 
 const TEXT_DELAY = 20000;
+// const TEXT_DELAY = 5000;
+
+const SERVER_PORT = 3002;
 
 const TicketSpinner = ({ history }) => {
   const [showText, setShowText] = useState(false);
+  const [showErrorText, setShowErrorText] = useState(false);
   // Go back to main screen after some time
   useEffect(() => {
     // TODO: wait until the ticket actually prints, assume ticket prints in 5 seconds
@@ -19,21 +23,35 @@ const TicketSpinner = ({ history }) => {
 
   // print ticket after some time
   useEffect(() => {
-    const printRelayTrigger = setTimeout(() => {
+    const printRelayClose = setTimeout(() => {
       setShowText(true);
+      // contact the server to close the switch
+      fetch(`http://localhost:${SERVER_PORT}/close`, { method: 'GET', mode: 'no-cors' }).catch(
+        error => {
+          console.log('Error printing ticket', error);
+          setShowErrorText(true);
+        }
+      );
     }, TEXT_DELAY);
     return () => {
-      clearTimeout(printRelayTrigger);
+      clearTimeout(printRelayClose);
     };
   });
 
-  // close the relay
+  // open the relay to stop the ticket printing
   useEffect(() => {
-    const printRelayClose = setTimeout(() => {
-      // Only open the print switch for 1 second
+    // Only open the print switch for 1 second
+    const printRelayOpen = setTimeout(() => {
+      fetch(`http://localhost:${SERVER_PORT}/open`, { method: 'GET', mode: 'no-cors' }).catch(
+        error => {
+          console.log('Error opening ticket relay', error);
+        }
+      );
     }, TEXT_DELAY + 1000);
+    // contact the server to open the switch
+
     return () => {
-      clearTimeout(printRelayClose);
+      clearTimeout(printRelayOpen);
     };
   }, []);
   return (
@@ -42,6 +60,11 @@ const TicketSpinner = ({ history }) => {
       {showText ? (
         <h2 className={classNames(styles.ticketText, styles.blink_me)}>
           Here&prime;s your ticket!
+        </h2>
+      ) : null}
+      {showErrorText ? (
+        <h2 className={classNames(styles.ticketText)}>
+          Error printing ticket! Please contact your airport gate agent.
         </h2>
       ) : null}
     </div>
